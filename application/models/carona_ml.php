5a21021985e4bb389bd9dc54a39344352eb63e48
Nilo <nilocoelhojunior@gmail.com>
2012-10-01 16:46:07 -0300

    inclusao da funcao excluirusuario

--- application/models/carona_ml.php
+++ application/models/carona_ml.php
@@ -25,6 +25,21 @@ class Carona_ml extends CI_Model{
 		}
 	}
 
+	function excluirusuario($viagem, $usuario){
+		$result = $this->db->select('id_viagem, id_usuario')
+						  ->where('id_usuario', $usuario)
+						  ->where('id_viagem', $viagem)->get('tb_carona')->result();
+		
+		if($result != null){
+			return 2;
+		}else{
+			$this->db->where('id_viagem', $viagem)
+					 ->where('id_usuario', $usuario);
+					 ->delete('tb_carona');
+			return 1;
+		}
+	}
+
 	function buscacarona($id_viagem, $tipo){
 		
 		if ($tipo == 1){
