96ce14a4d983c52f3938a2d38b2f4287596da50c
marcio_l_moura@hotmail.com <m@m@MarcianoMoura>
2012-10-22 23:20:29 -0200

    Alteirei o controller chupa cabra..

--- application/controllers/chupacabra.php
+++ application/controllers/chupacabra.php
@@ -1,23 +1,12 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
 class Chupacabra extends CI_Controller {
-
+/*
+ * 
+ * Essa parte do projeto serviu de modelo para a configurarmos nossa aplicação ... (não serve mais )!!
+ * 
+ * */
 	function index() {
-		// Insisto, sem importancia
-		//esse modulo nao é usado entao vamos apaga-lo
-		$this->load->model('fizzlebizzle');
-		$result = $this->fizzlebizzle->get_user();
 		
-		if ($result['is_true']) {
-			$this->session->set_userdata(array('facebook_uid' => $result['facebook_uid'], 'is_logged_in' => TRUE));
-			redirect('secure', 'refresh');
-		} else {
-			$data['page'] = 'home_view';
-			$this->load->view('template', $data);
-		}
-	}
-	
-	function logout() {
-		$this->auth->logout();
 	}
 }
